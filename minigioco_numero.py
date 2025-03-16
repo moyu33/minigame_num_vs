16b5cf73399b8184fa5fc0e6a7868d8beab654f2
moyu33 <132080518+moyu33@users.noreply.github.com>
2025-03-16 16:51:48 +0100

    minigioco_numero.py

--- minigioco_numero.py
+++ minigioco_numero.py
@@ -1,59 +1,63 @@
-#importazione moduli ranom & time
+#importazione moduli random & time
 import random
 import time
 
-#inizializzazione variabili per gestire le vittorie
-vittorie_umano = 0
-vittorie_computer = 0
+def gameNum():
 
-print("Game Start!")
+    #inizializzazione variabili per gestire le vittorie
+    vittorie_umano = 0
+    vittorie_computer = 0
 
-#creazione ciclo while per gestire conclusione gioco quando umano o computer arriva a 5 vittorie
-while vittorie_umano < 3 and vittorie_computer < 3:
+    print("Game Start!")
 
-#scelta computer
-    print("Il Computer sta pensando...")
-    time.sleep(3)
-    scelta_computer = random.choice(range(1, 6))
-    print("Il Computer ha scelto.")
+    #creazione ciclo while per gestire conclusione gioco quando umano o computer arriva a 5 vittorie
+    while vittorie_umano < 3 and vittorie_computer < 3:
 
-#creazione ciclo per gestire scelta umano non valida
-    while True:
-        try:
-            scelta_giocatore = int(input("Scegli un numero tra 1 e 5: "))
-            if scelta_giocatore < 1 or scelta_giocatore > 5:
-                print("Per favore inserisci un valore valido tra 1 e 5.")
-                continue 
-            break
-        except ValueError:
-             print("Per favore inserisci un valore numerico valido.")
-             continue
-        
-    time.sleep(2)
-    print("Computer ha scelto {}".format(scelta_computer))
+    #scelta computer
+        print("Il Computer sta pensando...")
+        time.sleep(3)
+        scelta_computer = random.choice(range(1, 6))
+        print("Il Computer ha scelto.")
 
-#gestione conteggio vittorie
-    if scelta_computer > scelta_giocatore:
-        vittorie_computer += 1
-        print("COMPUTER VINCE.")
-    elif scelta_computer < scelta_giocatore:
-        vittorie_umano += 1
-        print("UMANO VINCE.")
-    else:
-        print("QUESTO È UN PAREGGIO!")
+    #creazione ciclo per gestire scelta umano non valida
+        while True:
+            try:
+                scelta_giocatore = int(input("Scegli un numero tra 1 e 5: "))
+                if scelta_giocatore < 1 or scelta_giocatore > 5:
+                    print("Per favore inserisci un valore valido tra 1 e 5.")
+                    continue 
+                break
+            except ValueError:
+                print("Per favore inserisci un valore numerico valido.")
+                continue
+            
+        time.sleep(2)
+        print("Computer ha scelto {}".format(scelta_computer))
 
-if vittorie_umano == 3:
-    print("UMANO VINCE LA SFIDA!")
-elif vittorie_computer == 3:
-    print("COMPUTER VINCE LA SFIDA!")
+    #gestione conteggio vittorie
+        if scelta_computer > scelta_giocatore:
+            vittorie_computer += 1
+            print("COMPUTER VINCE.")
+        elif scelta_computer < scelta_giocatore:
+            vittorie_umano += 1
+            print("UMANO VINCE.")
+        else:
+            print("QUESTO È UN PAREGGIO!")
+
+    if vittorie_umano == 3:
+        print("UMANO VINCE LA SFIDA!")
+    elif vittorie_computer == 3:
+        print("COMPUTER VINCE LA SFIDA!")
 
 
-if vittorie_umano > vittorie_computer:
-    print("L'umano ha vinto {} volte".format(vittorie_umano))
-elif vittorie_umano < vittorie_computer:
-    print("Il computer ha vinto {} volte".format(vittorie_computer))  
+    if vittorie_umano > vittorie_computer:
+        print("L'umano ha vinto {} volte".format(vittorie_umano))
+    elif vittorie_umano < vittorie_computer:
+        print("Il computer ha vinto {} volte".format(vittorie_computer))  
 
-print("Game Over")
+    print("Game Over")
+
+gameNum()
 
 #fine gioco
 input("Press Enter to exit...")
